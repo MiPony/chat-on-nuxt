e5b76b45d0d5f77ea4ab0f51c2879cf759a2deb7
Oleg <peliho1129@gmail.com>
2020-05-06 19:41:59 +0300

    Nuxt-chat

---
+++ components/chatForm.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-col xs12>
+        <v-text-field
+        	label="Введите сообщение"
+            outlined
+            v-model="text"
+            @keydown.enter="send"
+        >
+        	
+        </v-text-field>
+    </v-col>
+</template>
+
+<script>
+export default{
+	data: () => ({
+		text: ''
+	}),
+	methods: {
+		send() {
+			this.$socket.emit('createMessage', {
+				text: this.text,
+				id: this.$store.state.user.id
+			}, data => {
+				if (typeof data === 'string') {
+					console.error(data);
+				} else {
+					this.text = "";
+				}
+			})
+		}
+	}
+}
+</script>
